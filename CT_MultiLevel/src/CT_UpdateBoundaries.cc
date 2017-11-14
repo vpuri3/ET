b61bc28c930231b55192af10873b93c8de68ef6e
Erik Schnetter <schnetter@gmail.com>
2017-11-14 10:14:40 -0500

    CT_Multilevel: Add explicit type conversions that are required by the new C++ standard

--- CT_MultiLevel/src/CT_UpdateBoundaries.cc
+++ CT_MultiLevel/src/CT_UpdateBoundaries.cc
@@ -15,14 +15,14 @@ extern "C" void CT_UpdateBoundaries(CCTK_ARGUMENTS, const char *varname)
   DECLARE_CCTK_ARGUMENTS;
   DECLARE_CCTK_PARAMETERS;
 
-  if (CCTK_Equals(veryverbose, "yes")) CCTK_VInfo(CCTK_THORNSTRING, "Updating boundaries of %s...", varname);
+  if (CCTK_Equals(veryverbose, "yes")) CCTK_VINFO("Updating boundaries of %s...", varname);
    
 //  BEGIN_MAP_LOOP(cctkGH,CCTK_GF) {
 //    BEGIN_COMPONENT_LOOP(cctkGH, CCTK_GF) {
 //      if (CT_ProcessOwnsData())
 //      {
         if (CCTK_SyncGroup(cctkGH, varname) < 0)
-          CCTK_VWarn(0, __LINE__, __FILE__, "Couldn't sync variable %s", varname); 
+          CCTK_VERROR("Couldn't sync variable %s", varname); 
 //      }
 //    } END_COMPONENT_LOOP;
 //  } END_MAP_LOOP;
@@ -34,7 +34,7 @@ extern "C" void CT_UpdateBoundaries(CCTK_ARGUMENTS, const char *varname)
   } else {
     int ierr = Boundary_SelectGroupForBC(cctkGH, CCTK_ALL_FACES, cctk_nghostzones[0], -1, varname, "none");
     if (ierr < 0)
-      CCTK_VWarn(0, __LINE__, __FILE__, "Failed to register boundary conditions for %s.", varname);
+      CCTK_VERROR("Failed to register boundary conditions for %s.", varname);
 
     BEGIN_MAP_LOOP(cctkGH,CCTK_GF) {
       BEGIN_COMPONENT_LOOP(cctkGH, CCTK_GF) {
