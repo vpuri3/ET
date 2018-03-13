929c6deb61edc82c45547df53f80c13c86ac7759
Roland Haas <rhaas@ncsa.illinois.edu>
2018-03-13 17:33:47 -0500

    CT_MultiLevel: add sample hook for PETSc

--- CT_MultiLevel/src/CT_SolvePsiEquation.cc
+++ CT_MultiLevel/src/CT_SolvePsiEquation.cc
@@ -9,6 +9,8 @@
 
 #include "CT_MultiLevel.hh"
 
+static void CT_PETScSolve(CCTK_ARGUMENTS);
+
 extern "C" void CT_SolvePsiEquation(CCTK_ARGUMENTS, CCTK_REAL *norm, CCTK_INT *step)
 {
   DECLARE_CCTK_PARAMETERS;
@@ -77,7 +79,11 @@ extern "C" void CT_SolvePsiEquation(CCTK_ARGUMENTS, CCTK_REAL *norm, CCTK_INT *s
               npoints
             };
             CT_InitializeCoefficients(sindex, nequation, &cptr[nequation], stnc);
+            // RH: the code above makes copies of the coefficients in
+            // CT_MultiLevel internal variables. No idea what they are used for
+            // but let's keep them for now.
 
+#if 0
             CCTK_REAL psix, psiy, psiz, psixx, psiyy, psizz, psixy, psixz, psiyz;
             CT_FD(ct_psi, stnc, nequation, 
                   &psix, &psiy, &psiz, &psixx, &psiyy, &psizz, &psixy, &psixz, &psiyz);
@@ -117,8 +123,11 @@ extern "C" void CT_SolvePsiEquation(CCTK_ARGUMENTS, CCTK_REAL *norm, CCTK_INT *s
                                              + ct_c3[index] * psi3
                                              + ct_c4[index] * psi4 
                                              );
+#endif
           }
         } LC_ENDLOOP3 (EL_RLX);
+
+        CT_PETScSolve(CCTK_PASS_CTOC);
 
         free(cptr);
       } // if
@@ -143,4 +152,58 @@ extern "C" void CT_SolvePsiEquation(CCTK_ARGUMENTS, CCTK_REAL *norm, CCTK_INT *s
   }
 
   return;
+}
+
+static void CT_PETScSolve(CCTK_ARGUMENTS)
+{
+  DECLARE_CCTK_PARAMETERS; // gets me everything in param.ccl
+  DECLARE_CCTK_ARGUMENTS; // gets me everything in interface.ccl
+
+  int xgh = cctk_nghostzones[0];
+  int ygh = cctk_nghostzones[1];
+  int zgh = cctk_nghostzones[2];
+  int imin = xgh;
+  int imax = cctk_lsh[0]-xgh;
+  int jmin = ygh;
+  int jmax = cctk_lsh[1]-ygh;
+  int kmin = zgh;
+  int kmax = cctk_lsh[2]-zgh;
+
+  // output some debug messages so that we know what is going on. Needs to be
+  // to stderr so that they all show up.
+  int myproc =  CCTK_MyProc(cctkGH);
+  fprintf(stderr, "Solving on reflevel %d component %d iteration %d on process %d\n"
+                  "Showing myproc, i,j,k, x,y,z, nequation, psi, ct_c0\n",
+          Carpet::reflevel, Carpet::component, cctk_iteration, myproc);
+
+  // number of points on the grid is nx * ny * nz
+  int npoints = cctk_lsh[0] * cctk_lsh[1] * cctk_lsh[2];
+
+  // RH: just a demo code to show how to access things. Your code must provide
+  // updated values for the unknown field ct_psi in the range
+  // xgh <= i < cctk_lsh[0] - xgh etc.
+  for(int nequation = 0 ; nequation < number_of_equations ; nequation++) {
+    for(int k = kmin ; k < kmax ; k++) {
+      for(int j = jmin ; j < jmax ; j++) {
+        for(int i = imin ; i < imax ; i++) {
+          // linear index into a 3d array
+          int index3d = i + j*cctk_lsh[0] + k*cctk_lsh[0]*cctk_lsh[1];
+          // linear index into an vector of arrays eg psi(i,j,k,nequation) in matlab
+          // that is the coefficients for the n'th equation (coutning from 0)
+          // start at linear index: nequation * npoints
+          int index4d = i + j*cctk_lsh[0] + k*cctk_lsh[0]*cctk_lsh[1] +
+                        nequation*cctk_lsh[0]*cctk_lsh[1]*cctk_lsh[2];
+
+          // you cannot use fprintf(stdout, ...) for output since only one
+          // process will output, but stderr will work.
+          fprintf(stderr, "%d %d %d %d %g %g %g %d %g %g\n",
+                  myproc, i,j,k, x[index3d],y[index3d],z[index3d], nequation,
+                  ct_psi[index4d], ct_c0[index4d]);
+        }
+      }
+    }
+  }
+
+  fprintf(stderr, "Done on reflevel %d component %d iteration %d on process %d\n",
+          Carpet::reflevel, Carpet::component, cctk_iteration, myproc);
 }
